42487279674843fbdbc6cf28c13b57791ca3384f
bragaus <sprtj@protonmail.com>
2022-02-28 06:46:18 -0300

    criação do cabeçalho principal

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div id="app">
+    <MainHeader />
   </div>
 </template>
 
 <script>
 import './styles/global.css'
+import MainHeader from './components/MainHeader'
 
 export default {
   name: 'App',
   components: {
+    MainHeader
   }
 }
 </script>
---
+++ src/components/MainHeader.vue
@@ -0,0 +1,39 @@
+<template>
+    <header>
+        <nav class="container">
+            <a href="/"><img src="https://raw.githubusercontent.com/william-costa/wdev-mock-site-resources/master/assets/images/wdev.svg" alt="WDEV" id="logo"></a>
+            <img src="https://raw.githubusercontent.com/william-costa/wdev-mock-site-resources/master/assets/images/menu.svg" alt="Abrir menu" id="menu-botton">
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'MainHeader'
+}
+</script>
+
+<!--scoped = Tudo que for escrito de css aqui estara restrito apenas para essa pagina-->
+<style scoped>
+    header {
+        background-color: var(--color-background-nav);
+        height: 60px;
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    nav {
+        display: flex;
+        justify-content: space-between;
+    }
+
+    #logo {
+        width: 130px;
+    }
+
+    #menu-botton{
+        width: 30px;
+    }
+</style>
