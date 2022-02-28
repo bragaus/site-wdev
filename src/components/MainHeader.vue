6cd0d30c364a7a78638550eacb9a46946e0f75fd
bragaus <sprtj@protonmail.com>
2022-02-28 07:30:10 -0300

    finalizado header mobile, aprendi sobre :class, v-if e v-on, gostei muito mais do que useState do react, facebook vai tomar no cuuuuu

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -2,14 +2,40 @@
     <header>
         <nav class="container">
             <a href="/"><img src="https://raw.githubusercontent.com/william-costa/wdev-mock-site-resources/master/assets/images/wdev.svg" alt="WDEV" id="logo"></a>
-            <img src="https://raw.githubusercontent.com/william-costa/wdev-mock-site-resources/master/assets/images/menu.svg" alt="Abrir menu" id="menu-botton">
+            <img v-on:click="lidarComMenu" src="https://raw.githubusercontent.com/william-costa/wdev-mock-site-resources/master/assets/images/menu.svg" alt="Abrir menu" id="menu-botton">
+            <div v-on:click="lidarComMenu" id="menu-overlay" v-if="menuActive"></div>
+
+            <!-- :class é uma indicação de codigo javascript pode passar uma função, objeto, etc para ser processado.
+            :class="{active:menuActive} se menuActive for verdadeiro, sera adicionado a classe active"-->
+            <div id="menu-items" :class="{active:menuActive}">
+
+                <img src="https://raw.githubusercontent.com/william-costa/wdev-mock-site-resources/master/assets/images/wdev.svg" alt="WDEV" id="menu-logo">
+
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/videos">Videos</a></li>
+                    <li><a href="/sobre">Sobre</a></li>
+                    <li><a href="/contato">Contato</a></li>
+                </ul>
+
+            </div>
         </nav>
     </header>
 </template>
 
 <script>
 export default {
-    name: 'MainHeader'
+    name: 'MainHeader',
+    data() {
+        return {
+            menuActive: false 
+        }
+    },
+    methods: {
+        lidarComMenu: function(){
+            this.menuActive = !this.menuActive;
+        }
+    }
 }
 </script>
 
@@ -35,5 +61,51 @@ export default {
 
     #menu-botton{
         width: 30px;
+    }
+
+    #menu-overlay {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 40%;
+        height: 100vh;
+        background-color: black;
+        opacity: 0.8;
+    }
+
+    #menu-logo {
+        width: 110px;
+        margin-top: 30px;
+        margin-bottom: 10px;
+    }
+
+    #menu-items {
+        position: fixed;
+        top: 0;
+        right: 0;
+        background-color: var(--color-background-nav);
+        width: 60%;
+        height: 100vh;
+        display: none;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: center;
+    }
+
+    #menu-items.active {
+        display: flex;
+    }
+
+    ul {
+        list-style: none;
+        text-align: center;
+    }
+
+    ul li a {
+        color: var(--color-text-light);
+    }
+
+    ul li {
+        margin: 20px 0px;
     }
 </style>
