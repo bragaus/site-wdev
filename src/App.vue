c29a5b1a9480bc76cdccaab715460756af1f236a
bragaus <sprtj@protonmail.com>
2022-03-01 11:18:49 -0300

    adicionado homepage

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <MainHeader />
+    <HomePage />
   </div>
 </template>
 
 <script>
 import './styles/global.css'
 import MainHeader from './components/MainHeader'
+import HomePage from './pages/HomePage'
 
 export default {
   name: 'App',
   components: {
-    MainHeader
+    MainHeader,
+    HomePage
   }
 }
 </script>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,18 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+export default {
+    name: 'HomePage',
+    methods: {
+
+    }
+}
+</script>
+
+<style scoped>
+
+    
+
+</style>
