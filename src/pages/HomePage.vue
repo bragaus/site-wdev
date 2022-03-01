8aff15aeed8d5a34e83b792c5f591f1ebfe29805
bragaus <sprtj@protonmail.com>
2022-03-01 14:23:07 -0300

    home page finalizada

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,5 +1,11 @@
 <template>
-    <div></div>
+    <main>
+        <div class="text-top">A Wired e o mundo real são a mesma coisa</div>
+        <a href="https://br.pinterest.com/lugaracidental/_saved/" target="_blank">
+            <img id="exodus" src="https://i.pinimg.com/originals/90/df/05/90df05c56feffe48d9fc42ff17c9af82.png" alt="a um passo da abdução">
+        </a>
+        <div class="text-bottom">Deitado na lua; afundado nas projeções; vi que tudo é passageiro.</div>
+    </main>
 </template>
 
 <script>
@@ -13,6 +19,31 @@ export default {
 
 <style scoped>
 
-    
+   main {
+       background-color: var(--color-background-home);
+       justify-content: center;
+       align-items: center;
+   }     
 
+    .text-top {
+        color: var(--color-text-light);
+        font-size: 22px;
+        margin-bottom: 20px;
+    }
+
+    .text-bottom {
+        color: var(--color-text-light);
+        font-size: 18px;
+        margin-top: 20px;
+    }
+
+    #exodus {
+        width: 100px;
+    }
+
+    @media (min-width: 700px) {
+        #exodus {
+            width: 300px;
+        }
+    }
 </style>
