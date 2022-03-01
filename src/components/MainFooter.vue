599417dce0cd39318cae934f52556c81d2e7a32a
bragaus <sprtj@protonmail.com>
2022-03-01 14:54:00 -0300

    primeiras impressão consumindo api do VUE

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -1,12 +1,29 @@
 <template>
-    <footer></footer>
+    <footer>
+
+        <!-- :key, :href, :alt... serve para passar um valor js para a propriedade -->
+        <a class="socialLink" v-for="(socialLink, index) in socialLinks" :key="index" :href="socialLink.link">
+            <img :title="socialLink.label" :src="socialLink.icon" :alt="socialLink.label">
+        </a>
+
+    </footer>
 </template>
 
 <script>
+import api from '@/services/api.js';
+
+// mounted é um evento que acontece no ciclo de vida da pagina, no caso esse é quando ela acabar de ser montada
 export default {
     name: 'MainFooter',
-    methods: {
-
+    data() {
+        return {
+            socialLinks: []
+        }
+    },
+    mounted() {
+        api.get('/social-links.json').then(response => {
+            this.socialLinks = response.data;
+        });
     }
 }
 </script>
