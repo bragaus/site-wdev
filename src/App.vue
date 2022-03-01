6350a0c11b955d96ab9da7ed7a446235e0b2aa0a
bragaus <sprtj@protonmail.com>
2022-03-01 14:35:07 -0300

    adicionado footer

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <MainHeader />
     <HomePage />
+    <MainFooter />
   </div>
 </template>
 
@@ -9,12 +10,14 @@
 import './styles/global.css'
 import MainHeader from './components/MainHeader'
 import HomePage from './pages/HomePage'
+import MainFooter from './components/MainFooter'
 
 export default {
   name: 'App',
   components: {
     MainHeader,
-    HomePage
+    HomePage,
+    MainFooter
   }
 }
 </script>
---
+++ src/components/MainFooter.vue
@@ -0,0 +1,25 @@
+<template>
+    <footer></footer>
+</template>
+
+<script>
+export default {
+    name: 'MainFooter',
+    methods: {
+
+    }
+}
+</script>
+
+<style scoped>
+
+    footer {
+        height: 60px;
+        width: 100%;
+        background-color: var(--color-background-nav);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+</style>
